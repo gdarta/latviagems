c94d8a4bc1493c4395c01ead52072d6ac45e1391
Raivis Randohs <118553404+phantzzy@users.noreply.github.com>
2025-04-07 16:21:50 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./docs/**/*.html",
+    "./docs/**/*.js" // just in case you use Tailwind classes in JS too
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
